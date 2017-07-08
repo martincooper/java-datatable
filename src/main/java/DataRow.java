6bc29189a4e8c954c1aa26bfc3ac39a48960445f
Martin Cooper <martin.cooper@me.com>
2017-07-08 14:45:37 +0100

    Initial project setup.

---
+++ src/main/java/DataRow.java
@@ -0,0 +1,6 @@
+/**
+ * A row of data in the DataTable.
+ * Created by Martin Cooper on 08/07/2017.
+ */
+public class DataRow {
+}
---
+++ src/main/java/DataTable.java
@@ -0,0 +1,6 @@
+/**
+ * DataTable. The main top level class.
+ * Created by Martin Cooper on 08/07/2017.
+ */
+public class DataTable {
+}
