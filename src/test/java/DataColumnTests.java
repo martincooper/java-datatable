fe156630547b9fba04fd55b7a25cd3a126399176
Martin Cooper <martin.cooper@me.com>
2017-07-13 18:27:35 +0200

    Implementing initial classes.

---
+++ src/test/java/DataColumnTests.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Unit Tests for the DataColumn class.
+ * Created by Martin on 13/07/2017.
+ */
+public class DataColumnTests {
+
+    @Test
+    public void testDataColumnCreation() {
+
+        DataColumn<String> column = new DataColumn<>(String.class, "StringCol");
+
+        assertEquals(column.getName(), "StringCol");
+        assertEquals(column.getColumnType(), String.class);
+
+    }
+
+}
