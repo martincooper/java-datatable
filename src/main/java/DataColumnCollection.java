c4f7fa17a794e3c43f1cd9c9df59515f605f3cca
Martin Cooper <martin.cooper@me.com>
2017-07-14 11:35:33 +0200

    Implementing Data Column Collection and Column validation.

--- src/main/java/DataColumnCollection.java
+++ src/main/java/DataColumnCollection.java
@@ -1,8 +1,5 @@
 import io.vavr.collection.List;
 import io.vavr.collection.Vector;
-import io.vavr.control.Try;
-
-import java.util.stream.Stream;
 
 /**
  * DataColumnCollection. Handles a collection of Data Columns.
@@ -13,32 +10,15 @@ public class DataColumnCollection {
     private final DataTable table;
     private final Vector<IDataColumn> columns;
 
+    public DataColumnCollection(DataTable table) {
+        this(table, List.empty());
+    }
+
     public DataColumnCollection(DataTable table, Iterable<IDataColumn> columns) {
         Guard.notNull(table, "table");
         Guard.itemsNotNull(columns, "columns");
 
         this.table = table;
         this.columns = Vector.ofAll(columns);
-    }
-
-    private Try validateColumns(Iterable<IDataColumn> columns) {
-        List<IDataColumn> dataCols = List.ofAll(columns);
-
-        return validateColumnNames(dataCols)
-                .map(x -> validateColumnDataLength(dataCols));
-    }
-
-    private Try<Void> validateColumnNames(List<IDataColumn> columns) {
-        if (columns.groupBy(IDataColumn::getName).length() != columns.length())
-            return Try.failure(new DataTableException("Columns contain duplicate names."));
-
-        return Try.success(null);
-    }
-
-    private Try validateColumnDataLength(List<IDataColumn> columns) {
-        if (List.ofAll(columns).groupBy(x -> x.getData().length()).length() > 1)
-            return Try.failure(new DataTableException("Columns have uneven row count."));
-
-        return Try.success(null);
     }
 }
--- src/main/java/DataTable.java
+++ src/main/java/DataTable.java
@@ -1,6 +1,52 @@
+import io.vavr.collection.List;
+import io.vavr.control.Try;
+
 /**
  * DataTable. The main top level class.
  * Created by Martin Cooper on 08/07/2017.
  */
 public class DataTable {
+
+    private final String name;
+    private final DataColumnCollection columns;
+
+    private DataTable(String tableName) {
+        this.name = tableName;
+        this.columns = new DataColumnCollection(this);
+    }
+
+    public static Try<DataTable> build(String tableName) {
+        return Try.success(new DataTable(tableName));
+    }
+
+    public static Try<DataTable> build(String tableName, Iterable<IDataColumn> columns) {
+
+        Try result = validateColumns(columns);
+
+        if (result.isFailure())
+            return Try.failure(result.getCause());
+
+        return Try.success(new DataTable(tableName));
+    }
+
+    private static Try<Void> validateColumns(Iterable<IDataColumn> columns) {
+        List<IDataColumn> dataCols = List.ofAll(columns);
+
+        return validateColumnNames(dataCols)
+                .andThen(x -> validateColumnDataLength(dataCols));
+    }
+
+    private static Try<Void> validateColumnNames(List<IDataColumn> columns) {
+        if (columns.groupBy(IDataColumn::getName).length() != columns.length())
+            return Try.failure(new DataTableException("Columns contain duplicate names."));
+
+        return Try.success(null);
+    }
+
+    private static Try<Void> validateColumnDataLength(List<IDataColumn> columns) {
+        if (List.ofAll(columns).groupBy(x -> x.getData().length()).length() > 1)
+            return Try.failure(new DataTableException("Columns have uneven row count."));
+
+        return Try.success(null);
+    }
 }
