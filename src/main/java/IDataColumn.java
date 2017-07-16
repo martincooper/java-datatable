31b9d19f45fcc9a7ff2c744566f4158c16586027
Martin Cooper <martin.cooper@me.com>
2017-07-16 19:45:58 +0200

    IDataColumn changed to extend IModifiable.

--- src/main/java/IDataColumn.java
+++ src/main/java/IDataColumn.java
@@ -1,62 +1,29 @@
 import io.vavr.collection.Vector;
-import io.vavr.control.Try;
-
 import java.lang.reflect.Type;
 
 /**
  * Generic interface for the DataColumn class.
+ * Allows columns that can store different data types to be stored
+ * in a generic collection without losing type specific information.
  * Created by Martin Cooper on 13/07/2017.
  */
-public interface IDataColumn {
+public interface IDataColumn extends IModifiable<Integer, Object, IDataColumn> {
 
     /**
-     *
+     * The column name.
      * @return Returns the column name.
      */
     String getName();
 
     /**
-     *
+     * The column type.
      * @return Returns the column type.
      */
     Type getType();
 
     /**
-     *
+     * The underlying data.
      * @return Returns access to the underlying data.
      */
     Vector getData();
-
-    /**
-     * Attempts to add / append a new item to the end of the column.
-     * A type check is performed before addition.
-     * @param value The item required to be added.
-     * @return Returns a Success with the new modified DataColumn, or a Failure.
-     */
-    Try<IDataColumn> add(Object value);
-
-    /**
-     * Attempts to insert a new item into the column.
-     * A type check is performed before insertion.
-     * @param index The index the item is to be inserted at.
-     * @param value The item required to be inserted.
-     * @return Returns a Success with the new modified DataColumn, or a Failure.
-     */
-    Try<IDataColumn> insert(Integer index, Object value);
-
-    /**
-     * Attempts to replace an existing item with a new item in the column.
-     * A type check is performed before replacement.
-     * @param index The index the item is to be replaced at.
-     * @param value The new item.
-     * @return Returns a Success with the new modified DataColumn, or a Failure.
-     */
-    Try<IDataColumn> replace(Integer index, Object value);
-
-    /**
-     * Attempts to remove an existing item at the specified index.
-     * @param index The index to remove the item at.
-     * @return Returns a Success with the new modified DataColumn, or a Failure.
-     */
-    Try<IDataColumn> remove(Integer index);
 }
