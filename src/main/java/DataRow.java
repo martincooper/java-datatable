b985b0a00a875338abb51e15c31ffc5e58fc162a
Martin Cooper <martin.cooper@me.com>
2017-07-17 11:18:06 +0200

    Adding Data Row functionality.

--- src/main/java/DataRow.java
+++ src/main/java/DataRow.java
@@ -1,6 +1,38 @@
+import io.vavr.control.Try;
+
 /**
  * A row of data in the DataTable.
  * Created by Martin Cooper on 08/07/2017.
  */
 public class DataRow {
+
+    private final DataTable table;
+    private final Integer rowIdx;
+
+    /**
+     * Private DataRow constructor.
+     * Use 'build' to create instance.
+     * @param table The DataTable the DataRow is pointing to.
+     * @param rowIdx The row index.
+     */
+    private DataRow(DataTable table, Integer rowIdx) {
+        this.table = table;
+        this.rowIdx = rowIdx;
+    }
+
+    /**
+     * Builds an instance of a DataRow.
+     * Row Index is validated before creation, returning a Failure on error.
+     * @param table The DataTable the DataRow is pointing to.
+     * @param rowIdx The row index.
+     * @return Returns a DataRow wrapped in a Try.
+     */
+    public static Try<DataRow> build(DataTable table, Integer rowIdx) {
+        Guard.notNull(table, "table");
+
+        // Check row index bounds, and return new DataRow if success.
+        return VectorExtensions.outOfBounds(table.rowCount(), rowIdx)
+                ? DataTableException.tryError("Invalid row index for DataRow.")
+                : Try.success(new DataRow(table, rowIdx));
+    }
 }
--- src/main/java/Guard.java
+++ src/main/java/Guard.java
@@ -1,4 +1,5 @@
 import io.vavr.collection.Stream;
+import io.vavr.control.Try;
 
 import java.util.Objects;
 
@@ -41,5 +42,16 @@ public class Guard {
     public static <T> void itemsNotNull(T[] argument, String name) {
         notNull(argument, name);
         itemsNotNull(Stream.of(argument), name);
+    }
+
+    /**
+     * Asserts the argument is not null. Returns in a Try.
+     * @param argument The argument to check.
+     * @param name The name of the argument.
+     */
+    public static <T> Try<T> tryNotNull(Object argument, String name) {
+        return argument == null
+                ? Try.failure(new IllegalArgumentException("Invalid value [NULL] for argument " + name))
+                : Try.success(null);
     }
 }
