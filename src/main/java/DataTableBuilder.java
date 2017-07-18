0fe3f9986d15accaaea91d17316dd5a30f64c5d5
Martin Cooper <martin.cooper@me.com>
2017-07-18 21:19:01 +0200

    Adding DataTableBuilder class and tests.

--- src/main/java/DataTableBuilder.java
+++ src/main/java/DataTableBuilder.java
@@ -8,28 +8,58 @@ import io.vavr.control.Try;
 public class DataTableBuilder {
 
     private final String tableName;
-    private final List<IDataColumn> dataColumns;
+    private List<IDataColumn> dataColumns;
 
-    public DataTableBuilder(String tableName) {
+    /**
+     * DataTableBuilder. Private constructor. Used by the create() method.
+     * @param tableName The name of the table to create.
+     */
+    private DataTableBuilder(String tableName) {
         this.tableName = tableName;
         this.dataColumns = List.empty();
     }
 
-    public static DataTableBuilder build(String tableName) {
+    /**
+     * Creates a new instance of a DataTableBuilder, passing table name.
+     * @param tableName The name of the table.
+     * @return Returns a new instance of a DataTableBuilder, allowing method chaining.
+     */
+    public static DataTableBuilder create(String tableName) {
         return new DataTableBuilder(tableName);
     }
 
+    /**
+     * Allows an additional column to be added when building a table.
+     * @param type The data type of the column.
+     * @param columnName The column name.
+     * @param data The data contained in the column.
+     * @param <T> The column type.
+     * @return Adds a new column and returns an instance to the current Data Table Builder.
+     */
     public <T> DataTableBuilder withColumn(Class<T> type, String columnName, Iterable<T> data) {
-        this.dataColumns.append(new DataColumn<T>(type, columnName, data));
+        this.dataColumns = this.dataColumns.append(new DataColumn<T>(type, columnName, data));
         return this;
     }
 
-    public <T> DataTableBuilder withColumn(Class<T> type, String columnName, T... data) {
-        this.dataColumns.append(new DataColumn<T>(type, columnName, data));
+    /**
+     * Allows an additional column to be added when building a table.
+     * @param type The data type of the column.
+     * @param columnName The column name.
+     * @param data The data contained in the column.
+     * @param <T> The column type.
+     * @return Adds a new column and returns an instance to the current Data Table Builder.
+     */
+    @SafeVarargs
+    public final <T> DataTableBuilder withColumn(Class<T> type, String columnName, T... data) {
+        this.dataColumns = this.dataColumns.append(new DataColumn<T>(type, columnName, data));
         return this;
     }
 
-    public Try<DataTable> create() {
+    /**
+     * Attempts to build the data table from all the details in the chained method calls.
+     * @return Returns a Try DataTable.
+     */
+    public Try<DataTable> build() {
         return DataTable.build(this.tableName, this.dataColumns);
     }
 }
--- src/test/java/DataTableBuilderTests.java
+++ src/test/java/DataTableBuilderTests.java
@@ -6,31 +6,35 @@ import static org.junit.Assert.*;
 
 /**
  * Tests for the Data Table Builder.
- * Created by Martin on 18/07/2017.
+ * Created by Martin Cooper on 18/07/2017.
  */
 public class DataTableBuilderTests {
 
     @Test
     public void testBuilderDataTableCreationFromIterable() {
         Try<DataTable> table = DataTableBuilder
-                .build("NewTable")
-                .withColumn(String.class, "StrCol", List.of("AA", "BB", "CC"))
-                .withColumn(Integer.class, "IntCol", List.of(3, 5, 9))
-                .withColumn(Boolean.class, "BoolCol", List.of(true, false, true))
-                .create();
+                .create("NewTable")
+                .withColumn(String.class, "StrCol", List.of("AA", "BB", "CC", "DD"))
+                .withColumn(Integer.class, "IntCol", List.of(3, 5, 9, 11))
+                .withColumn(Boolean.class, "BoolCol", List.of(true, false, true, false))
+                .build();
 
         assertTrue(table.isSuccess());
+        assertTrue(table.get().columns().count() == 3);
+        assertTrue(table.get().rowCount() == 4);
     }
 
     @Test
     public void testBuilderDataTableCreationFromVarArgs() {
         Try<DataTable> table = DataTableBuilder
-                .build("NewTable")
-                .withColumn(String.class, "StrCol", "AA", "BB", "CC")
-                .withColumn(Integer.class, "IntCol", 3, 5, 9)
-                .withColumn(Boolean.class, "BoolCol", true, false, true)
-                .create();
+                .create("NewTable")
+                .withColumn(String.class, "StrCol", "AA", "BB", "CC", "DD")
+                .withColumn(Integer.class, "IntCol", 3, 5, 9, 11)
+                .withColumn(Boolean.class, "BoolCol", true, false, true, false)
+                .build();
 
         assertTrue(table.isSuccess());
+        assertTrue(table.get().columns().count() == 3);
+        assertTrue(table.get().rowCount() == 4);
     }
 }
