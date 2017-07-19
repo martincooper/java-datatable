830a58366e57337f8b3d6bace53bb9a1c52e1c3e
Martin Cooper <martin.cooper@me.com>
2017-07-19 12:02:10 +0200

    Adding memory performance tests.

--- src/test/profiling/DataTableMemoryTests.java
+++ src/test/profiling/DataTableMemoryTests.java
@@ -1,11 +1,10 @@
-import io.vavr.collection.List;
 import io.vavr.collection.Stream;
 import io.vavr.control.Try;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.Random;
 
-import static org.junit.Assert.assertTrue;
 
 /**
  * Memory Tests for the Data Table.
@@ -14,50 +13,64 @@ import static org.junit.Assert.assertTrue;
 public class DataTableMemoryTests {
 
     private final Random rand = new Random();
+    private static Integer ROW_COUNT = 1000000;
 
+    @Ignore
     @Test
-    public void testBuilderDataTableCreationFromIterable() {
-        Try<DataTable> table = DataTableBuilder
-                .create("NewTable")
-                .withColumn(String.class, "StrCol", List.of("AA", "BB", "CC", "DD"))
-                .withColumn(Integer.class, "IntCol", List.of(3, 5, 9, 11))
-                .withColumn(Boolean.class, "BoolCol", List.of(true, false, true, false))
-                .build();
+    public void testRawRowData() {
+        String[] stringData = randomStringData(ROW_COUNT);
+        Integer[] intData = randomIntegerData(ROW_COUNT);
+        Double[] doubleData = randomDoubleData(ROW_COUNT);
+        Boolean[] boolData = randomBooleanData(ROW_COUNT);
 
-        assertTrue(table.isSuccess());
-        assertTrue(table.get().columns().count() == 3);
-        assertTrue(table.get().rowCount() == 4);
+        long mem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+        System.out.println("MB: " + (mem / 1024) / 1024);
     }
 
-    private Integer[] createRandomIntegerData(Long dataSize) {
+    @Ignore
+    @Test
+    public void testDataTableWithRowData() {
+        Try<DataTable> table = DataTableBuilder
+                .create("NewTable")
+                .withColumn(String.class, "StrCol", randomStringData(ROW_COUNT))
+                .withColumn(Integer.class, "IntCol", randomIntegerData(ROW_COUNT))
+                .withColumn(Double.class, "DoubleCol", randomDoubleData(ROW_COUNT))
+                .withColumn(Boolean.class, "BoolCol", randomBooleanData(ROW_COUNT))
+                .build();
+
+        long mem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+        System.out.println("MB: " + (mem / 1024) / 1024);
+    }
+
+    private Integer[] randomIntegerData(int dataSize) {
         return Stream
                 .range(0, dataSize)
                 .map(i -> rand.nextInt())
                 .toJavaArray(Integer.class);
     }
 
-    private Boolean[] createRandomBooleanData(Long dataSize) {
+    private Boolean[] randomBooleanData(int dataSize) {
         return Stream
                 .range(0, dataSize)
                 .map(i -> rand.nextBoolean())
                 .toJavaArray(Boolean.class);
     }
 
-    private Double[] createRandomDoubleData(Long dataSize) {
+    private Double[] randomDoubleData(int dataSize) {
         return Stream
                 .range(0, dataSize)
                 .map(i -> rand.nextDouble())
                 .toJavaArray(Double.class);
     }
 
-    private String[] createRandomStringData(Long dataSize) {
+    private String[] randomStringData(int dataSize) {
         return Stream
                 .range(0, dataSize)
-                .map(i -> createRandomString(10))
+                .map(i -> randomString(10))
                 .toJavaArray(String.class);
     }
 
-    private String createRandomString(Integer length) {
+    private String randomString(int length) {
         return Stream
                 .range(0, length)
                 .map(i -> (char)(rand.nextInt((int)(Character.MAX_VALUE))))
