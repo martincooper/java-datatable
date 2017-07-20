0d284653d4c2132066033b08fa98a486505e1e73
Martin Cooper <martin.cooper@me.com>
2017-07-20 15:35:16 +0200

    Adding / Updating filtering functionality.

--- src/test/examples/Filtering.java
+++ src/test/examples/Filtering.java
@@ -9,9 +9,9 @@ import static org.junit.Assert.*;
  */
 public class Filtering {
 
-    //@Ignore
+    @Ignore
     @Test
-    public void testDataTableWithRowData() {
+    public void testUntypedDataTableFilterByColumnName() {
         DataTable table = DataTableBuilder
                 .create("NewTable")
                 .withColumn(String.class, "StrCol", "AA", "BB", "AA", "BB")
@@ -20,8 +20,22 @@ public class Filtering {
                 .withColumn(Boolean.class, "BoolCol", true, true, false, false)
                 .build().get();
 
-        DataView view = table.filter(row -> "BB".equals(row.get(0)));
+        DataView view = table.filter(row -> (boolean)row.get("BoolCol"));
+        assertTrue(view.rowCount() == 2);
+    }
 
+    @Ignore
+    @Test
+    public void testTypedDataTableFilterByColumnName() {
+        DataTable table = DataTableBuilder
+                .create("NewTable")
+                .withColumn(String.class, "StrCol", "AA", "BB", "AA", "BB")
+                .withColumn(Integer.class, "IntCol", 1, 100, 1000, 10000)
+                .withColumn(Double.class, "DoubleCol", 1.1, 5.5, 10.5, 100.5)
+                .withColumn(Boolean.class, "BoolCol", true, true, false, false)
+                .build().get();
+
+        DataView view = table.filter(row -> row.getAs(Integer.class, "IntCol") > 100);
         assertTrue(view.rowCount() == 2);
     }
 }
