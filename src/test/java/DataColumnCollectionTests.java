1b60306122f2667d88767dd001e7acf545a70042
Martin Cooper <martin.cooper@me.com>
2017-07-25 14:04:41 +0100

    Updating Unit Tests.

--- src/test/java/DataColumnCollectionTests.java
+++ src/test/java/DataColumnCollectionTests.java
@@ -23,6 +23,52 @@ public class DataColumnCollectionTests {
         assertTrue(newTable.get().columns().get(3).name().equals("DoubleCol"));
     }
 
+    @Test
+    public void testRemoveColumn() {
+        DataTable oldTable = createDataTable();
+
+        Try<DataTable> newTable = oldTable.columns().remove("IntegerCol");
+
+        assertTrue(newTable.isSuccess());
+        assertTrue(oldTable.columns().count() == 3);
+        assertTrue(newTable.get().columns().count() == 2);
+        assertTrue(newTable.get().columns().get(1).name().equals("BooleanCol"));
+    }
+
+    @Test
+    public void testInsertColumn() {
+        DataTable oldTable = createDataTable();
+
+        IDataColumn newCol = createDoubleColumn();
+        Try<DataTable> newTable = oldTable.columns().insert(0, newCol);
+
+        assertTrue(newTable.isSuccess());
+        assertTrue(oldTable.columns().count() == 3);
+        assertTrue(newTable.get().columns().count() == 4);
+
+        // Check the column was inserted in the correct place.
+        assertTrue(newTable.get().columns().get(0).name().equals("DoubleCol"));
+        assertTrue(newTable.get().columns().get(1).name().equals("StringCol"));
+        assertTrue(newTable.get().columns().get(2).name().equals("IntegerCol"));
+    }
+
+    @Test
+    public void testReplaceColumn() {
+        DataTable oldTable = createDataTable();
+
+        IDataColumn newCol = createDoubleColumn();
+        Try<DataTable> newTable = oldTable.columns().replace("IntegerCol", newCol);
+
+        assertTrue(newTable.isSuccess());
+        assertTrue(oldTable.columns().count() == 3);
+        assertTrue(newTable.get().columns().count() == 3);
+
+        // Check the correct column was replaced.
+        assertTrue(newTable.get().columns().get(0).name().equals("StringCol"));
+        assertTrue(newTable.get().columns().get(1).name().equals("DoubleCol"));
+        assertTrue(newTable.get().columns().get(2).name().equals("BooleanCol"));
+    }
+
     private DataTable createDataTable() {
         IDataColumn[] cols = { createStringColumn(), createIntegerColumn(), createBooleanColumn() };
         return DataTable.build("NewTable", cols).get();
