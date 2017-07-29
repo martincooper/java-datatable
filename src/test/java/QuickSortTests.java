9518abcc9c5643768c9bb1586b3cefb79ea9580f
Martin Cooper <martin.cooper@me.com>
2017-07-29 14:36:02 +0100

    Implementing sorting.

--- src/test/java/QuickSortTests.java
+++ src/test/java/QuickSortTests.java
@@ -1,8 +1,11 @@
 import com.github.martincooper.datatable.DataTable;
 import com.github.martincooper.datatable.DataTableBuilder;
 import com.github.martincooper.datatable.DataView;
+import com.github.martincooper.datatable.sorting.SortItem;
 import com.github.martincooper.datatable.sorting.SortOrder;
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import io.vavr.collection.List;
+import io.vavr.collection.Stream;
 import io.vavr.control.Try;
 import org.junit.Test;
 
@@ -34,5 +37,32 @@ public class QuickSortTests {
         assertTrue(view.get().row(4).getAs(Integer.class, "IntCol") == 3);
         assertTrue(view.get().row(5).getAs(Integer.class, "IntCol") == 0);
         assertTrue(view.get().row(6).getAs(Integer.class, "IntCol") == -30);
+    }
+
+    @Test
+    public void testMultiQuickSort() {
+
+        DataTable table = DataTableBuilder
+                .create("NewTable")
+                .withColumn(Integer.class, "IndexCol", List.of(1, 2, 3, 4, 5, 6, 7))
+                .withColumn(Integer.class, "NumberCol", List.of(2, 3, 3, 4, 4, 5, 5))
+                .withColumn(String.class, "StringCol", List.of("aa", "bb", "cc", "dd", "ee", "ff", "gg"))
+                .build().get();
+
+        SortItem sortOne = new SortItem("NumberCol", SortOrder.Ascending);
+        SortItem sortTwo = new SortItem("StringCol", SortOrder.Descending);
+
+        Try<DataView> view = table.quickSort(Stream.of(sortOne, sortTwo));
+
+        assertTrue(view.isSuccess());
+
+        // Check Index Column values are as expected.
+        assertTrue(view.get().row(0).getAs(Integer.class, "IndexCol") == 1);
+        assertTrue(view.get().row(1).getAs(Integer.class, "IndexCol") == 3);
+        assertTrue(view.get().row(2).getAs(Integer.class, "IndexCol") == 2);
+        assertTrue(view.get().row(3).getAs(Integer.class, "IndexCol") == 5);
+        assertTrue(view.get().row(4).getAs(Integer.class, "IndexCol") == 4);
+        assertTrue(view.get().row(5).getAs(Integer.class, "IndexCol") == 7);
+        assertTrue(view.get().row(6).getAs(Integer.class, "IndexCol") == 6);
     }
 }
