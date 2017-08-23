5b2cb3df114df4c3046d2bd350e1fedb4f9a253d
Martin Cooper <martin.cooper@me.com>
2017-08-23 14:20:53 +0100

    Adding row add, insert, replace, remove.

--- src/main/java/com/github/martincooper/datatable/DataRowCollectionModifiable.java
+++ src/main/java/com/github/martincooper/datatable/DataRowCollectionModifiable.java
@@ -30,6 +30,38 @@ public class DataRowCollectionModifiable extends DataRowCollectionBase {
     }
 
     /**
+     * Returns a new DataTable with the additional row inserted at the specified index.
+     *
+     * @param idx The row index.
+     * @param rowValues The values to insert into the row.
+     * @return Returns a new DataTable with the row inserted.
+     */
+    public Try<DataTable> insert(int idx, Object[] rowValues) {
+        return null;
+    }
+
+    /**
+     * Returns a new DataTable with the data replaced at the specified index.
+     *
+     * @param idx The row index.
+     * @param rowValues The new values to replaced the old ones.
+     * @return Returns a new DataTable with the row inserted.
+     */
+    public Try<DataTable> replace(int idx, Object[] rowValues) {
+        return null;
+    }
+
+    /**
+     * Returns a new DataTable with the specified row removed.
+     *
+     * @param idx The row index.
+     * @return Returns a new DataTable with the row removed.
+     */
+    public Try<DataTable> remove(int idx) {
+        return null;
+    }
+
+    /**
      * Builds a new DataRowCollection for the specified DataTable.
      * @param table The table to build the DataRowCollection for.
      * @return Returns the DataRowCollection.
